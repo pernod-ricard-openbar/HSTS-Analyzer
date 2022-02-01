909cdd3602d26c53043aaaaf7ecf21c16c1c7b4e
Laurent Bel <laurent.bel@pernod-ricard.com>
2022-02-01 12:12:18 +0100

    Grade enum like class

---
+++ api/models/HstsGrade.cs
@@ -0,0 +1,12 @@
+namespace Hsts{
+
+    public class HstsGrade {
+        public const string APlus = "A+";
+        public const string A = "A";
+        public const string B = "B";
+        public const string C = "C";
+        public const string D = "D";
+        public const string E = "E";
+        public const string F = "F";
+    }
+}
